70e6991ba954632b4c4b8e9de10d1c2b4347f1a5
Leroy Rosales <leroyrosales@gmail.com>
2021-03-22 19:48:01 -0500

    Fixes typo

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,7 +1,7 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import resume from '../files/LRosales_resume.pdf'
+import resume from '../files/LRosales_Resume.pdf'
 
 const Resume = () => (
 
