9ab7e7e32a2b416324df05b4af07010e5819a16f
Leroy Rosales <leroyrosales@gmail.com>
2021-03-26 13:53:01 -0500

    Comments out public dir from gatsby config

---
+++ public/styles-c63c59d8051425665640.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{OEcT:function(n,o,w){},Vr40:function(n,o,w){},wmEu:function(n,o,w){}}]);
+//# sourceMappingURL=styles-c63c59d8051425665640.js.map
