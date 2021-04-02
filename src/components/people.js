55b888c0db0d364cfc3220e4e5025479cb04b668
Leroy Rosales <leroyrosales@gmail.com>
2021-04-01 20:15:14 -0500

    Adds some new faces to my people list

--- src/components/people.js
+++ src/components/people.js
@@ -13,6 +13,10 @@ const People = () => (
             <li><a className="text-green-600 hover:text-green-800 hover:underline" href="https://firehazard.co/" target="_blank">Fire Hazard</a></li>
             <li><a className="text-green-600 hover:text-green-800 hover:underline" href="https://www.chriswiegman.com/" target="_blank">Chris Weigman</a></li>
             <li><a className="text-green-600 hover:text-green-800 hover:underline" href="https://jacarvalho.com/" target="_blank">Alex Carvalho</a></li>
+            <li><a className="text-green-600 hover:text-green-800 hover:underline" href="https://eroberts.me/" target="_blank">Ethan Roberts</a></li>
+            <li><a className="text-green-600 hover:text-green-800 hover:underline" href="https://adambos.co/" target="_blank">Adam Bosco</a></li>
+            <li><a className="text-green-600 hover:text-green-800 hover:underline" href="https://www.tbare.com/" target="_blank">Tim Bare</a></li>
+            <li><a className="text-green-600 hover:text-green-800 hover:underline" href="https://wpinfusion.com/" target="_blank">Christian Gelici</a></li>
           </ul>
         </div>
       </section>
